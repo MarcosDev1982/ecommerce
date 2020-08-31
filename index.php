be16760016da1bd233cb449d3fd33d081f0219cb
Marcos Alan da Silva Barbosa <66331285+Marcosjedai@users.noreply.github.com>
2020-08-30 22:11:54 -0300

    erro ao edita

--- index.php
+++ index.php
@@ -61,6 +61,79 @@ exit;
 
 });
 
+$app->get("/admin/users", function() {
+
+	User::verifyLogin();
+
+	$users = User::listALL();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+
+
+
+});
+
+$app->get("/admin/users/create", function(){ 
+	
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+	
+	$page->setTpl("users-create");
+	
+	});
+
+	
+	$app->get("/admin/users/:idusers/delete", function($idusers){
+
+        User::verifyLogin();
+
+
+	}); 
+
+	$app->get("/admin/users/:iduser", function($iduser){ 
+	
+		User::verifyLogin();
+		$user = new User();
+ 
+		$user->get((int)$iduser);
+	    $page = new PageAdmin();
+		$page ->setTpl("users-update", array(
+			"user"=>$user->getValues()
+		));
+
+	});
+
+	$app->post("/admin/users/create", function(){
+
+        User::verifyLogin();
+		
+		$user = new User();
+		$_POST["inadmin"]= (isset($_POST["inadmin"]))?1:0;
+		
+		$user->setData($_POST);
+
+		$user->save();
+
+		header("Location: /admin/users");
+		exit;
+
+	}); 
+
+
+	$app->post("admin/users/:idusers", function($idusers){
+
+        User::verifyLogin();
+
+
+	}); 
+
+
+
 
 
 
